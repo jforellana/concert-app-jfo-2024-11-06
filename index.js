9834950777db8bb276e4bb95ff08522d998d88ee
Juan <mrjfob@byu.edu>
2024-11-06 22:09:34 +0000

    inserted html page

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let express = require("express");
-let app = new express();                                                                     
+let app = express();
 
 // set up database connection
 const knex = require("knex")({
  client: "mysql",
  connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
+  host:"concert-db.cl8qs2ywg9rc.us-east-2.rds.amazonaws.com",
   user: "admin",
-  password: "Password1",
+  password: "ATpSDeIO0uqdniHfmitP",
   database:"paradise-concerts",
   port: 3306,
  },
@@ -18,8 +18,19 @@ app.get("/",(req,res) => {
  .select()
  .from("venues")
  .then((result) => {
-  console.log(result);
-  res.send(result);
- }); 
+	let html = "<body><ul>";
+	for (let i=0;i<result.length;i++) {
+		html += "<li>" + result[i].location + "</li>";
+	}
+	html += "</body>"
+  res.send(html);
+ })
+.catch((error) => {
+	console.error(error);
+	res.status(500).send("Error getting data from db");
 });
-app.listen(3000);
+});
+
+app.listen(3000, () => {
+	console.log("listening");
+});
